b69e25ae8dc29888c74a424e993971ac0a7c21d9
fanxing <18404985916@163.com>
2019-10-18 13:23:03 +0800

    inintal project

---
+++ test/unit/webpack-base-test.js
@@ -0,0 +1,11 @@
+
+const assert = require('assert');
+
+describe('webpack.base.js test case', () => {
+    const baseConfig = require('../../lib/webpack.base.js')
+
+    it('entry', () => {
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-fx/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-fx/test/smoke/template/src/search/index.js') > -1, true);
+    });
+});
